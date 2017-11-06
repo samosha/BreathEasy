e0880d1d8eecbde9e30d910939e22af63f796add
Andrii S <andrii.dp.ua@gmail.com>
2017-11-06 20:37:40 +0200

    +string on main.js

--- WCK/main.js
+++ WCK/main.js
@@ -24,3 +24,35 @@ me=ye<18?'Sorry, you very molodoy':'ok, you are welcome';
 alert(me);
 */
 
+/* Уроки со строками :) 
+
+
+console.log('Some string');
+console.log("this console");
+console.log('test "test"tsets ');
+
+console.log('Hello'.length);
+console.log("conasol \"fdsfs \\ ");
+var string = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore explicabo repellat repellendus maxime blanditiis. Quo eveniet non assumenda';
+console.log(string.charAt(0));
+console.log(string.charAt(1));
+console.log(string.charAt(2));
+console.log(string.charAt(3));
+console.log(string.charAt(4));
+console.log(string.charAt(string.length-1));
+
+console.log(string.slice(-10));
+console.log(string.substring(0,5));
+console.log(string.substr(0,5));
+
+console.log(string.indexOf("amet"));
+console.log(string.lastIndexOf("amet"));
+
+
+console.log(string.split(" "))
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+console.log(string[4].toUpperCase());
+ */
+
+ /* логические значения  */
