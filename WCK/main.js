bcc0c4d82b090873ef9dc02eb41481bc555aa3d3
Andrii S <andrii.dp.ua@gmail.com>
2017-11-06 17:46:46 +0200

    +sima and notsima

---
+++ WCK/main.js
@@ -0,0 +1,26 @@
+/*alert("Hello World");
+var message = 'Hello, my name is Andrii';
+var age = 28;
+var merried = true;
+merried = false;
+
+alert(age);
+alert(message);
+
+var x = null;
+var u;
+u = undefined;
+
+var test = 89;
+alert(test++);
+alert(test);
+
+var ye = prompt('сколько лет,','18');
+
+var con = confirm('вам точно 18 ?');
+
+var me; 
+me=ye<18?'Sorry, you very molodoy':'ok, you are welcome';
+alert(me);
+*/
+
